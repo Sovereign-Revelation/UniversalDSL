d150c363a57812e25df9554ee5f40e0876a777be
iconoclast <iconoclastdao@gmail.com>
2025-05-27 12:11:56 -0500

    Update apiController.js

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -4,19 +4,33 @@ const fs = require('fs');
 
 exports.runFlow = (req, res) => {
   const flowJSON = req.body;
+
   const tmpFile = tmp.fileSync({ postfix: '.jsonflow' });
   fs.writeFileSync(tmpFile.name, JSON.stringify(flowJSON, null, 2));
 
+  let output = '';
   const originalLog = console.log;
-  let output = "";
-  console.log = (msg) => output += msg + '\n';
+
+  // Override console.log to capture execution output
+  console.log = (msg) => {
+    if (typeof msg === 'object') {
+      output += JSON.stringify(msg, null, 2) + '\n';
+    } else {
+      output += msg + '\n';
+    }
+  };
 
   try {
-    require('../src/utils/jsonflow-executor').executeJsonFlow(tmpFile.name);
-    res.send(output);
+    executeJsonFlow(tmpFile.name); // Execute flow
+    res.status(200).send(output || '✅ Flow executed with no output.');
   } catch (err) {
-    res.status(500).send(err.message);
+    res.status(500).json({
+      success: false,
+      message: err.message,
+      stack: process.env.NODE_ENV === 'development' ? err.stack : undefined,
+    });
   } finally {
-    console.log = originalLog;
+    console.log = originalLog; // Restore logging
+    tmpFile.removeCallback(); // Clean up tmp file
   }
 };
